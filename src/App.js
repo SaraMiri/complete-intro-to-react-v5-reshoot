267537573fe0389ee064820ba9d73b1293469cb7
Sky Nes <saramiragaya@gmail.com>
2020-01-02 16:51:26 +0100

    Starting with App Component

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    { id: "something-important" },
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Perdita",
+      animal: "Dog",
+      breed: "Mixed"
+    }),
+    React.createElement(Pet, {
+      name: "Lume",
+      animal: "Dog",
+      breed: "Labrador"
+    }),
+    React.createElement(Pet, {
+      name: "Pancita",
+      animal: "Hamster",
+      breed: "Sirio"
+    })
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
