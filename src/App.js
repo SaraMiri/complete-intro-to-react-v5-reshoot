96c1ca2c289ef600e485a8e058fb71cbf276bee7
Sky Nes <saramiragaya@gmail.com>
2020-01-04 00:21:53 +0100

    Using Jsx on App

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "something-important" },
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Perdita",
-      animal: "Dog",
-      breed: "Mixed"
-    }),
-    React.createElement(Pet, {
-      name: "Lume",
-      animal: "Dog",
-      breed: "Labrador"
-    }),
-    React.createElement(Pet, {
-      name: "Pancita",
-      animal: "Hamster",
-      breed: "Sirio"
-    })
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Perdita" animal="Dog" breed="Mixed" />
+      <Pet name="Lume" animal="Dog" breed="Labrador" />
+      <Pet name="Pancita" animal="Hamster" breed="Syrian" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
