373d6fd58603ce228f1e2ea355ead3d31896b044
Sky Nes <saramiragaya@gmail.com>
2020-01-04 13:26:17 +0100

    Add select animal

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("San Francisco, CA");
+  const [animal, setAnimal] = useState("dog");
   return (
     <div className="search-params">
       <form>
@@ -13,11 +15,28 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
             onChange={event => {
-              //This curly braces before setLocation are optional on this case
+              //This curly braces before setLocation are optional in this case
               setLocation(event.target.value);
             }}
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
